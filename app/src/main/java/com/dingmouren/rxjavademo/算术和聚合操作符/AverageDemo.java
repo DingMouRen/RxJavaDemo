78096c9507ff8cfb33db814c38a6afc8a2154a92
dingmouren <1341520108@qq.com>
2016-12-21 17:46:22 +0800

    添加rxjava-math

---
+++ app/src/main/java/com/dingmouren/rxjavademo/算术和聚合操作符/AverageDemo.java
@@ -0,0 +1,20 @@
+package com.dingmouren.rxjavademo.算术和聚合操作符;
+
+import rx.Observable;
+import rx.functions.Action1;
+
+/**
+ * Created by dingmouren on 2016/12/21.
+ * count操作符计算源Observable发射数据项的数量，将这个数量提交给订阅者
+ */
+
+public class AverageDemo {
+    public static void main(String[] args){
+        Observable.just(1,2,3).count().subscribe(new Action1<Integer>() {
+            @Override
+            public void call(Integer integer) {
+                System.out.println("count() + onNext:" + integer);
+            }
+        });
+    }
+}
